4a0b8fcec8844e7b0e9f2adbcaa815ae5ae81c08
Ainhoa Martinez <amartineziribas@gmail.com>
2022-03-14 18:59:23 -0700

    Conecta

---
+++ client/src/components/Messages/Message/Message.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import './Message.css';
+
+import ReactEmoji from 'react-emoji';
+
+const Message = ({ message: { text, user }, name }) => {
+  let isSentByCurrentUser = false;
+console.log(user, name, 'user - name')
+  const trimmedName = name.trim().toLowerCase();
+  console.log(trimmedName, 'trim')
+
+  if(user === trimmedName) {
+    isSentByCurrentUser = true;
+  }
+
+  return (
+    isSentByCurrentUser
+      ? (
+        <div className="messageContainer justifyEnd">
+          <p className="sentText pr-10">{trimmedName}</p>
+          <div className="messageBox backgroundBlue">
+            <p className="messageText colorWhite">{ReactEmoji.emojify(text)} eres {name}</p>
+          </div>
+        </div>
+        )
+        : (
+          <div className="messageContainer justifyStart">
+            <div className="messageBox backgroundLight">
+              <p className="messageText colorDark">{ReactEmoji.emojify(text)} no lo has escrito tu</p>
+            </div>
+            <p className="sentText pl-10 ">{user}</p>
+          </div>
+        )
+  );
+}
+
+export default Message;
